78d8c17b54ac1c626d80b0196a0f8db27e7fdb7f
Farshad Davari <DavariFarshad3@outlook.com>
2024-05-19 18:44:04 +0330

    Add first step of functionality.

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+// Variables
+let calculation = "";
+const outputResult = document.getElementById("result");
+const lastUpdateOutput = document.getElementById("last-update");
+
+// EventListeners
+
+
+// Functions
+
