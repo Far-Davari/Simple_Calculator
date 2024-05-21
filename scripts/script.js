1539dde7e826786824fa60d6a1e07daed2dbf161
Farshad Davari <DavariFarshad3@outlook.com>
2024-05-21 16:30:01 +0330

    Save to local storage.

--- scripts/script.js
+++ scripts/script.js
@@ -5,13 +5,21 @@ const lastUpdateOutput = document.getElementById("last-update");
 
 // Functions
 
+showLocal();
+
 const updateOutput = (value) => {
-    outputResult.innerText = value;
+  outputResult.innerText = value;
 }
 
 const calculateFunc = (value) => {
   const finalResult = calculation += value;
   console.log(finalResult);
   updateOutput(finalResult);
+}
+
+function showLocal() {
+  let localValue = `latest calculation: ${localStorage.getItem("calculation")}` || "there is no recent calculation";
+  lastUpdateOutput.innerHTML = `latest calculation: ${localStorage.getItem("calculation")}` || "there is no recent calculation";
+  console.log(localValue);
 }
 
