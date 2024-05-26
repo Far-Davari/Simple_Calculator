231ec9d86e7e0c5d63bd29462ae871d3e49fce5c
Farshad Davari <DavariFarshad3@outlook.com>
2024-05-26 16:45:40 +0330

    comment out "console.log"s.

--- scripts/script.js
+++ scripts/script.js
@@ -13,13 +13,13 @@ const updateOutput = (value) => {
 
 const calculateFunc = (value) => {
   const finalResult = calculation += value;
-  console.log(finalResult);
+  // console.log(finalResult);
   updateOutput(finalResult);
 }
 
 function showLocal() {
   let localValue = `latest calculation: ${localStorage.getItem("calculation")}` || "there is no recent calculation";
   lastUpdateOutput.innerHTML = `latest calculation: ${localStorage.getItem("calculation")}` || "there is no recent calculation";
-  console.log(localValue);
+  // console.log(localValue);
 }
 
